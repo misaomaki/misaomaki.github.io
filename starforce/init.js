da30b737db1ea33dc7d54e7f8850502d1974cec4
misaomaki <50785141+misaomaki@users.noreply.github.com>
2020-07-18 18:06:31 -0700

    u

--- starforce/init.js
+++ starforce/init.js
@@ -6,14 +6,17 @@ $(function() {
     for (let i = 0; i < s.length; ++i) {
         let i_s = s[i].innerHTML;
 
-        let i_img_match = i_s.match(/background: url\((.*)\);/gi);
+        let i_img_match = i_s.match(/background: url\((.*)\);/gi) || [];
+        let i_img_match2 = i_s.match(/background-image: url\((.*)\);/gi) || [];
+
+        let i_img_m = [...i_img_match, ...i_img_match2];
         
-        let i_img_l = i_img_match == null ? 0 : i_img_match.length;
+        let i_img_l = i_img_m.length;
         
         if (i_img_l === 0) continue;
 
         for (let j = 0; j < i_img_l; ++j) {
-            let j_img = i_img_match[j].replace("background: url(","").replace(");","");
+            let j_img = i_img_m[j].replace("background: url(","").replace("background-image: url(", "").replace(");","");
 
             ip.html(`
                 <img src="${j_img}">
