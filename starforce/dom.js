45bcba78ccf277c5410ffc57aa8f3ba731af8848
misaomaki <50785141+misaomaki@users.noreply.github.com>
2022-02-05 17:39:40 -0800

    autocube

--- starforce/dom.js
+++ starforce/dom.js
@@ -2440,26 +2440,30 @@ $(function() {
             //remove duplicates from the stat options
             let _s = stats[i];
 
-            let stat_options = _s.map(function(a) {
-                let sid = a[0];
-                let _st = cube.stat_upgrade(this_item.level,sid); //line stat verbiage
-
-                return `
-                    <option value="${sid}">${_st}</option>
-                `;
-            });
+            let stat_options = "";
 
             if (o.wildcard) {
-                stat_options.push(`
-                <option value="-1">&lt;Any Line&gt;</option>
-                `);
+                stat_options += `
+                    <option value="-1">&lt;Any Line&gt;</option>
+                `;
             }
+
+            stat_options = _s.reduce(function(a,b) {
+                let sid = b[0];
+                let _st = cube.stat_upgrade(this_item.level,sid); //line stat verbiage
+
+                a += `
+                    <option value="${sid}">${_st}</option>
+                `;
+
+                return a;
+            }, stat_options);
 
             tier_html += `
                 <div class="pot-stat-con" style="padding:5px">
                     <span class="pot-stat-line">Line ${idx}:</span>
                     <select id="cube_stat_line_${type}_${idx}" class="select-cube-line-${type}" data-id="${idx}">
-                        ${stat_options.join("")}
+                        ${stat_options}
                     </select>
                 </div>
             `;
