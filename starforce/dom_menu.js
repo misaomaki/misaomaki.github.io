9ecc32854a16d655c9f271a4b02ef10f4e731b63
misaomaki <50785141+misaomaki@users.noreply.github.com>
2026-06-10 21:48:35 -0700

    Fix on non-armor flames.

--- starforce/dom_menu.js
+++ starforce/dom_menu.js
@@ -587,7 +587,7 @@ $(function() {
                         
                         return self.hasClass(`item-weapon`);
                     }).removeClass("hidden");
-                } else if (this_item.class === "armor") {
+                } else if (this_item.class === "armor" || this_item.class === "accessory") {
                     all_box.filter(function() {
                         let __this = $(this);
                         let data = __this.data();
